043b930b1b7c8c4944663f1b513c59acbeac59b0
themrusgame <45094736+themrusgame@users.noreply.github.com>
2021-06-27 20:42:28 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Вызов модального окна
+$('.button').click( function() {
+	$('.overlay').fadeIn();
+});
+
+// Закрытие окна на крестик
+$('.close-popup').click( function() {
+	$('.overlay').fadeOut();
+});
+
+// Закрытие окна на поле
+$(document).mouseup( function (e) { 
+	var popup = $('.popup');
+	if (e.target != popup[0] && popup.has(e.target).length === 0){
+		$('.overlay').fadeOut();
+	}
+});
